4d4d8714fc6d4914821bf7a44e1823ec4928c1e6
Ketan Singh <ketansingh_vsec@yahoo.com>
2019-04-08 01:02:17 +0530

    working poc-1

--- nest.py
+++ nest.py
@@ -44,15 +44,15 @@ def magic(level=1, *args):
     frame = {}
     print("\nlevel=", level, "  args= ", args[0])
     if len(args[0]) < 1:
-        print("returning...nothing")
-        return
+        print("returning...[amount]")
+        return ['amount k:v']
 
     #above leaf node still
-    if len(args[0]) > 1:
-        for _t in args[0]:
-            if not _t in frame.keys():
-                d = magic(level+1, tuple(_tt(args)[1:]))
-                frame[_t] = d
+    if len(args[0]) >= 1:
+        for _argKey in args[0]:
+            if not _argKey in frame.keys():
+                d = magic(level + 1, tuple(_trimArgs(args)[1:]))
+                frame[_argKey] = d
             break
     elif len(args[0]) == 1:
         print("leaf node reached, attach as a list")
@@ -60,77 +60,106 @@ def magic(level=1, *args):
         frame = []
         frame.append(args[0][0])
 
-    print("frame=", frame)
     # list_args = [x for x in args[0]]
     # magic("bb", tuple(list_args[1:]))
 
-    print("returning...", frame)
+    print("returning...frame=", frame)
     return frame
 
 
-def _tt(arg):
+def _trimArgs(arg):
     list_args = [x for x in arg[0]]
     return list_args
 
+master_json = {}
 
-def filler(frame, table_data):
+def data_preparer(frame, table_data):
+    global master_json
+    for row in table_data:
+        d = filler(frame, row)
+        print("\nd=", d, "\n")
+
+        if len(master_json.keys()) == 0:
+            master_json = d
+        elif len(d.keys()) == 1:  #has to be one only
+            master_json_cpy = master_json
+            x = data_presser(d, master_json_cpy)
+            print("x=", x)
+            master_json = x
+        else:
+            print("ERROR!!!!!")
+    print("final master_json=", master_json)
+
+def data_presser(d, master_json_cpy):
+    # global master_json      #filled up
+
+    if isinstance(d, dict):
+        d_key = list(d.keys())[0]
+        if d_key in master_json_cpy.keys():     #key is same
+            d_cpy = d[d_key]
+            # master_json_cpy_ = master_json_cpy[list(master_json_cpy.keys())[d_key]]
+            master_json_cpy_ = master_json_cpy[d_key]
+            master_json_cpy[d_key] = data_presser(d_cpy, master_json_cpy_)
+
+        else:                                   #key is different, add
+            new_d = {}
+            new_d.update(d)
+            new_d.update(master_json_cpy)
+            return new_d
+    elif isinstance(d, list):
+        return master_json_cpy.append(d)
+
+    return master_json_cpy
+
+
+    # if list(d.keys())[0] in master_json.keys():
+    #     print("pressed master_data=")
+    #     master_json[list(d.keys())[0]][list(d.keys())[0]] = d[list(d.keys())[0]][list(d.keys())[0]]
+    #     print(master_json)
+    # else:
+    #     print("--new key in new row")
+    #     d_only_key = list(d.keys())[0]
+    #     master_json[d_only_key] = d[d_only_key]
+
+
+def filler(reduced_frame, table_row):
     global COLUMN_MAP
-    print(">frame=", frame)
+    # print(">frame=", reduced_frame)
+    # print(">row=", table_row)
     data = {}
-    l_data = []
 
-    row_counter = 0
-    while row_counter < len(table_data):
-        print("row_counter=", row_counter)
-        if isinstance(frame, dict):
-            for key in frame.keys():        #theres only one key
-                key_pos = COLUMN_MAP[key]
-                key_data = table_data[row_counter][key_pos]
-                print("key=", key, " key_pos=", key_pos, "data=", key_data)
-                _val = frame[key]     #new trimmed frame
-                if isinstance(_val, dict):
-                    data[key_data] = filler(_val, table_data)   #filling empty dict
-                    print("unreach")
-                    break
-                elif isinstance(_val, list):
-                    data[key_data] = filler(_val, table_data)  # filling empty dict
-                    # key_pos_ = COLUMN_MAP[_val[0]]
-                    # key_data_ = table_data[row_counter][key_pos_]
-                    # print("key=", _val[0], " key_pos=", key_pos_, "data=", key_data_)
-                    # data = []
-                    # data.append(key_data_)
-                    # return data
+    if isinstance(reduced_frame, dict):
+        for key in reduced_frame.keys():        #there has to be only one key
+            key_pos = COLUMN_MAP[key]
+            key_data = table_row[key_pos]
+            # print("key=", key, " key_pos=", key_pos, "data=", key_data)
+            _val = reduced_frame[key]           #new trimmed frame
+            data[key_data] = filler(_val, table_row)
 
-        elif isinstance(frame, list):   #last node leaf
-            print("......compute last node leaf")
-            _val = frame[0]
-            key_pos_ = COLUMN_MAP[_val]
-            key_data_ = table_data[row_counter][key_pos_]
-            print("key=", _val, " key_pos=", key_pos_, "data=", key_data_)
-            l_data.append(key_data_)
-            # return data
+    elif isinstance(reduced_frame, list):   #last node leaf
+        # print("......compute last node leaf")
+        key_pos_ = COLUMN_MAP['amount']
+        key_data_ = table_row[key_pos_]
+        # print("key=", 'amount', " key_pos=", key_pos_, "data=", key_data_)
+        leaf_data = []
+        data['amount'] = key_data_
+        leaf_data.append(data)
+        return leaf_data
 
-        row_counter += 1
-
-    if l_data:
-        print("\n", l_data)
-        return l_data
-    print("\n", data)
     return data
 
 
 
 
-
 if __name__ == "__main__":
-    filename = "input_2.json"
+    filename = "input.json"
     table_data = main(filename)
     # print(table_data)
     column_mapper()
 
     # frame = magic(1, ["country", "city", "country", "currency"])
-    frame = magic(1, ["city", "country"])
+    frame = magic(1, ["currency", "country", "city"])
     print("> final frame = ", frame)
 
     print("\n>inside filler...")
-    filler(frame, table_data)
+    data_preparer(frame, table_data)
